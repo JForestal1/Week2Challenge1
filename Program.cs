dcb68676d0fd3a62303920f85373e8bd8f78efc4
JForestal1 <jforestal1@gmail.com>
2020-11-18 23:26:26 -0500

    commit message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Week2Challenge1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Greeter Greet1 = new Greeter();
+            Greet1.SayHello("Joe");
+            Greet1.ProperSalutation("Stephanie");
+            Greet1.SayGoodbye("Stan");
+        }
+    }
+}
